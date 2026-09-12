97c61a48d3bddcbdbcfd5872ec5c645d9e561dcf
skriewel <s.kriewel@gmail.com>
2026-09-12 16:22:30 +0200

    custom: expose tap filters in cocktail search

--- src/views/CocktailsView.vue
+++ src/views/CocktailsView.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
 import CocktailIndex from "../components/Cocktail/CocktailIndex.vue";
+import CocktailTapListFilters from "../components/Cocktail/CocktailTapListFilters.vue";
+
+const mounted = ref(false);
+onMounted(() => {
+    mounted.value = true;
+});
 </script>
 
 <template>
     <main>
         <CocktailIndex />
+        <Teleport v-if="mounted" to=".resource-search__refinements__body">
+            <CocktailTapListFilters />
+        </Teleport>
     </main>
 </template>
