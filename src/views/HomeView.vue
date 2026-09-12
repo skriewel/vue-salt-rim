69bfe66c1f24122563c59649cbb80dd33443727e
skriewel <s.kriewel@gmail.com>
2026-09-12 16:10:36 +0200

    custom: show tap statistics on homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import Index from "../components/Shelf/ShelfIndex.vue";
+import TapStats from "../components/Shelf/TapStats.vue";
 </script>
 
 <template>
     <main>
         <Index />
+        <div class="shelf-container">
+            <TapStats />
+        </div>
     </main>
 </template>
