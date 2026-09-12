2c2b32a64415d6d86e3636d3742bfbfe04e4660c
skriewel <s.kriewel@gmail.com>
2026-09-12 16:25:56 +0200

    fix: show cocktail tap filters directly

--- src/views/CocktailsView.vue
+++ src/views/CocktailsView.vue
@@ -1,19 +1,11 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
 import CocktailIndex from "../components/Cocktail/CocktailIndex.vue";
 import CocktailTapListFilters from "../components/Cocktail/CocktailTapListFilters.vue";
-
-const mounted = ref(false);
-onMounted(() => {
-    mounted.value = true;
-});
 </script>
 
 <template>
     <main>
+        <CocktailTapListFilters />
         <CocktailIndex />
-        <Teleport v-if="mounted" to=".resource-search__refinements__body">
-            <CocktailTapListFilters />
-        </Teleport>
     </main>
 </template>
