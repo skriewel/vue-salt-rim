4a92ba5cd5371b29f7f682d6b5b39e5b1bf17d73
skriewel <s.kriewel@gmail.com>
2026-09-12 13:03:16 +0200

    custom: use member inventory for makeable cocktail filter

--- src/components/Cocktail/CocktailIndex.vue
+++ src/components/Cocktail/CocktailIndex.vue
@@ -136,14 +136,6 @@
                         @change="updateRouterPath"
                     ></Refinement>
                     <Refinement
-                        id="missing-ingredients"
-                        v-model="activeFilters.missing_ingredients as any"
-                        :title="$t('missing-ingredients') + ' (' + $t('shelf.title') + ')'"
-                        :refinements="refineMissingIngredients"
-                        type="radio"
-                        @change="updateRouterPath"
-                    ></Refinement>
-                    <Refinement
                         id="user-rating"
                         v-model="activeFilters.user_rating_min as any"
                         :title="$t('your-rating')"
@@ -186,7 +178,6 @@
                         <option value="name">{{ $t("name") }}</option>
                         <option value="created_at">{{ $t("date-added") }}</option>
                         <option value="favorited_at">{{ $t("date-favorited") }}</option>
-                        <option value="missing_ingredients">{{ $t("missing-ingredients") }}</option>
                         <option value="missing_bar_ingredients">{{ $t("missing-ingredients") }} ({{ $t("bars.bar") }})</option>
                         <option value="total_ingredients">{{ $t("total.ingredients") }}</option>
                         <option value="average_rating">{{ $t("average-rating") }}</option>
@@ -210,7 +201,7 @@
                     <button type="button" class="button button--input" :title="$t('cocktail.randomize')" @click.prevent="goToRandomCocktail">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
                             <path
-                                d="M10.9979 1.58018C11.6178 1.22132 12.3822 1.22132 13.0021 1.58018L20.5021 5.92229C21.1197 6.27987 21.5 6.93946 21.5 7.65314V16.3469C21.5 17.0606 21.1197 17.7202 20.5021 18.0778L13.0021 22.4199C12.3822 22.7788 11.6178 22.7788 10.9979 22.4199L3.49793 18.0778C2.88029 17.7202 2.5 17.0606 2.5 16.3469V7.65314C2.5 6.93947 2.88029 6.27987 3.49793 5.92229L10.9979 1.58018ZM4.5 7.65314V7.65792L11.0021 11.4223C11.6197 11.7799 12 12.4395 12 13.1531V20.689L19.5 16.3469V7.65314L12 3.31104L4.5 7.65314ZM6.13208 12.3C6.13206 11.7477 5.74432 11.0761 5.26604 10.7999C4.78776 10.5238 4.40004 10.7476 4.40006 11.2999C4.40008 11.8522 4.78782 12.5238 5.2661 12.7999C5.74439 13.0761 6.1321 12.8523 6.13208 12.3ZM8.72899 18.7982C9.20728 19.0743 9.59499 18.8505 9.59497 18.2982C9.59495 17.7459 9.20721 17.0743 8.72893 16.7982C8.25065 16.522 7.86293 16.7459 7.86295 17.2982C7.86297 17.8504 8.25071 18.522 8.72899 18.7982ZM5.2661 16.799C5.74439 17.0751 6.1321 16.8513 6.13208 16.299C6.13206 15.7467 5.74432 15.0751 5.26604 14.799C4.78776 14.5228 4.40004 14.7467 4.40006 15.2989C4.40008 15.8512 4.78782 16.5228 5.2661 16.799ZM8.72851 14.7995C9.20679 15.0756 9.5945 14.8518 9.59448 14.2995C9.59446 13.7472 9.20673 13.0756 8.72844 12.7995C8.25016 12.5233 7.86245 12.7471 7.86246 13.2994C7.86248 13.8517 8.25022 14.5233 8.72851 14.7995ZM14.8979 8.00001C15.3762 7.72388 15.3762 7.27619 14.8979 7.00006C14.4196 6.72394 13.6441 6.72394 13.1658 7.00006C12.6875 7.27619 12.6875 7.72388 13.1658 8.00001C13.6441 8.27614 14.4196 8.27614 14.8979 8.00001ZM10.0981 7.00006C10.5764 7.27619 10.5764 7.72388 10.0981 8.00001C9.61982 8.27614 8.84434 8.27614 8.36604 8.00001C7.88774 7.72388 7.88774 7.27619 8.36604 7.00006C8.84434 6.72394 9.61982 6.72394 10.0981 7.00006ZM15.9954 15.3495C16.5932 15.0043 17.0779 14.1649 17.0779 13.4745C17.0779 12.7842 16.5933 12.5044 15.9955 12.8496C15.3977 13.1948 14.9131 14.0342 14.913 14.7246C14.913 15.4149 15.3976 15.6947 15.9954 15.3495Z"
+                                d="M10.9979 1.58018C11.6178 1.22132 12.3822 1.22132 13.0021 1.58018L20.5021 5.92229C21.1197 6.27987 21.5 6.93946 21.5 7.65314V16.3469C21.5 17.0606 21.1197 17.7202 20.5021 18.0778L13.0021 22.4199C12.3822 22.7788 11.6178 22.7788 10.9979 22.4199L3.49793 18.0778C2.88029 17.7202 2.5 17.0606 2.5 16.3469V7.65314C2.5 6.93947 2.88029 6.27987 3.49793 5.92229L10.9979 1.58018ZM4.5 7.65314V7.65792L11.0021 11.4223C11.6197 11.7799 12 12.4395 12 13.1531V20.689L19.5 16.3469V7.65314L12 3.31104L4.5 7.65314ZM6.13208 12.3C6.13206 11.7477 5.74432 11.0761 5.26604 10.7999C4.78776 10.5238 4.40004 10.7476 4.40006 11.2999C4.40008 11.8522 4.78782 12.5238 5.2661 12.7999C5.74439 13.0761 6.1321 12.8523 6.13208 12.3ZM8.72899 18.7982C9.20728 19.0743 9.59499 18.8505 9.59497 18.2982C9.59495 17.7459 9.20721 17.0743 8.72893 16.7982C8.25065 16.522 7.86293 16.7459 7.86295 17.2982C7.86297 17.8504 8.25071 18.522 8.72899 18.7982ZM5.2661 16.799C5.74439 17.0751 6.1321 16.8513 6.13208 16.299C6.13206 15.7467 5.74432 15.0751 5.26604 14.799C4.78776 14.5228 4.40004 14.7467 4.40006 15.2989C4.40008 15.8512 4.78782 16.5228 5.2661 16.799ZM8.72851 14.7995C9.20679 15.0756 9.5945 14.8518 9.59448 14.2995C9.59446 13.7472 9.20673 15.0756 8.72844 14.7995C8.25016 14.5233 7.86245 14.7471 7.86246 15.2994C7.86248 15.8517 8.25022 16.5233 8.72851 16.7995ZM14.8979 8.00001C15.3762 7.72388 15.3762 7.27619 14.8979 7.00006C14.4196 6.72394 13.6441 6.72394 13.1658 7.00006C12.6875 7.27619 12.6875 7.72388 13.1658 8.00001C13.6441 8.27614 14.4196 8.27614 14.8979 8.00001ZM10.0981 7.00006C10.5764 7.27619 10.5764 7.72388 10.0981 8.00001C9.61982 8.27614 8.84434 8.27614 8.36604 8.00001C7.88774 7.72388 7.88774 7.27619 8.36604 7.00006C8.84434 6.72394 9.61982 6.72394 10.0981 7.00006ZM15.9954 15.3495C16.5932 15.0043 17.0779 14.1649 17.0779 13.4745C17.0779 12.7842 16.5933 12.5044 15.9955 12.8496C15.3977 13.1948 14.9131 14.0342 14.913 14.7246C14.913 15.4149 15.3976 15.6947 15.9954 15.3495Z"
                             ></path>
                         </svg>
                     </button>
@@ -268,7 +259,7 @@
                     <button v-show="totalActiveRefinements > 0" type="button" class="button button--input" :title="$t('clear-filters')" @click.prevent="clearRefinements">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
                             <path
-                                d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM12 10.5858L14.8284 7.75736L16.2426 9.17157L13.4142 12L16.2426 14.8284L14.8284 16.2426L12 13.4142L9.17157 16.2426L7.75736 14.8284L10.5858 12L7.75736 9.17157L9.17157 7.75736L12 10.5858Z"
+                                d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.23858 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM12 10.5858L14.8284 7.75736L16.2426 9.17157L13.4142 12L16.2426 14.8284L14.8284 16.2426L12 13.4142L9.17157 16.2426L7.75736 14.8284L10.5858 12L7.75736 9.17157L9.17157 7.75736L12 10.5858Z"
                             ></path>
                         </svg>
                     </button>
@@ -301,6 +292,7 @@ import { useRoute, useRouter } from "vue-router";
 import { useI18n } from "vue-i18n";
 import OverlayLoader from "./../OverlayLoader.vue";
 import BarAssistantClient from "@/api/BarAssistantClient";
+import MemberInventoryClient from "@/api/MemberInventoryClient";
 import CocktailGridItem from "./CocktailGridItem.vue";
 import CocktailGridContainer from "./CocktailGridContainer.vue";
 import PageHeader from "./../PageHeader.vue";
@@ -394,6 +386,8 @@ const meta = ref<any>({});
 const queryTimer = ref<number | null>(null);
 const currentPage = ref(1);
 const per_page = ref(50);
+const inventoryIngredientIds = ref<number[]>([]);
+const loadedInventoryId = ref<number | null>(null);
 
 const availableRefinements = ref<AvailableRefinements>({
     global: [],
@@ -590,10 +584,9 @@ function initializeGlobalRefinements() {
     availableRefinements.value.global = [
         { name: t("bar_shelf.cocktails"), active: false, id: "bar_shelf" },
         { name: t("bar_shelf.locked_cocktails"), active: false, id: "locked_bar_cocktails" },
-        { name: t("shelf.cocktails"), active: false, id: "on_shelf" },
+        { name: "Cocktails I can make", active: false, id: "on_shelf" },
         { name: t("my-favorites"), active: false, id: "favorites" },
         { name: t("cocktail.shared"), active: false, id: "is_public" },
-        { name: t("shelf.locked_cocktails"), active: false, id: "locked_user_cocktails" },
     ];
 
     availableRefinements.value.abv = [
@@ -607,12 +600,6 @@ function initializeGlobalRefinements() {
         { name: ">= " + t("n-ingredients", 3), active: false, id: "3" },
         { name: ">= " + t("n-ingredients", 5), active: false, id: "5" },
         { name: ">= " + t("n-ingredients", 7), active: false, id: "7" },
-    ];
-
-    availableRefinements.value.missing_ingredients = [
-        { name: t("n-ingredients", 1), active: false, id: "1" },
-        { name: t("n-ingredients", 2), active: false, id: "2" },
-        { name: ">= " + t("n-ingredients", 3), active: false, id: "3" },
     ];
 
     availableRefinements.value.missing_bar_ingredients = [
@@ -652,6 +639,32 @@ function fetchRefinements() {
     });
 }
 
+async function ensureInventoryIngredients() {
+    const requestedInventoryId = Number(route.query.inventory_id ?? 0);
+    if (requestedInventoryId > 0 && loadedInventoryId.value === requestedInventoryId) {
+        return;
+    }
+    if (requestedInventoryId === 0 && loadedInventoryId.value !== null) {
+        return;
+    }
+
+    const inventories = (await MemberInventoryClient.getInventories(appState.user.id)).data ?? [];
+    const inventory =
+        inventories.find((item) => requestedInventoryId > 0 && item.id === requestedInventoryId) ??
+        inventories.find((item) => item.name.toLowerCase() === "my shelf") ??
+        inventories[0] ??
+        null;
+
+    if (!inventory) {
+        loadedInventoryId.value = null;
+        inventoryIngredientIds.value = [];
+        return;
+    }
+
+    loadedInventoryId.value = inventory.id;
+    inventoryIngredientIds.value = (await MemberInventoryClient.getIngredients(appState.user.id, inventory.id)).data.map((ingredient) => ingredient.id);
+}
+
 function updateRouterPath() {
     const query = stateToQuery();
 
@@ -660,8 +673,21 @@ function updateRouterPath() {
     });
 }
 
-function refreshCocktails() {
+function serverQuery() {
     const query = stateToQuery();
+    delete query.inventory;
+    delete query.inventory_id;
+
+    if (activeFilters.value.on_shelf) {
+        query.filter = query.filter ?? {};
+        query.filter.shelf_ingredients = inventoryIngredientIds.value.length > 0 ? inventoryIngredientIds.value.join(",") : "-1";
+    }
+
+    return query;
+}
+
+function refreshCocktails() {
+    const query = serverQuery();
     query.include = "ratings,ingredients.ingredient,tags,images";
 
     isLoading.value = true;
@@ -685,7 +711,7 @@ function handlePageChange(toPage: number) {
 }
 
 function queryToState() {
-    const state = qs.parse(window.location.search.replace(/^\?/, "")) as ServerQueryFilters;
+    const state = qs.parse(window.location.search.replace(/^\?/, "")) as ServerQueryFilters & { inventory?: string; inventory_id?: string };
 
     activeFilters.value.tag_id = state.filter && state.filter.tag_id ? String(state.filter.tag_id).split(",") : [];
     activeFilters.value.cocktail_method_id = state.filter && state.filter.cocktail_method_id ? String(state.filter.cocktail_method_id).split(",") : [];
@@ -696,14 +722,14 @@ function queryToState() {
     activeFilters.value.created_user_id = state.filter && state.filter.created_user_id ? String(state.filter.created_user_id).split(",") : [];
     activeFilters.value.author = state.filter && state.filter.author ? String(state.filter.author).split(",") : [];
     activeFilters.value.favorited_by_user = state.filter && state.filter.favorited_by_user ? String(state.filter.favorited_by_user).split(",") : [];
-    activeFilters.value.on_shelf = state.filter && state.filter.on_shelf ? state.filter.on_shelf : null;
+    activeFilters.value.on_shelf = String(state.inventory ?? "") === "1";
     activeFilters.value.bar_shelf = state.filter && state.filter.bar_shelf ? state.filter.bar_shelf : null;
     activeFilters.value.locked_bar_cocktails = state.filter && state.filter.locked_bar_cocktails ? state.filter.locked_bar_cocktails : null;
-    activeFilters.value.locked_user_cocktails = state.filter && state.filter.locked_user_cocktails ? state.filter.locked_user_cocktails : null;
+    activeFilters.value.locked_user_cocktails = false;
     activeFilters.value.favorites = state.filter && state.filter.favorites ? state.filter.favorites : null;
     activeFilters.value.is_public = state.filter && state.filter.is_public ? state.filter.is_public : null;
     activeFilters.value.total_ingredients = state.filter && state.filter.total_ingredients ? String(state.filter.total_ingredients) : null;
-    activeFilters.value.missing_ingredients = state.filter && state.filter.missing_ingredients ? String(state.filter.missing_ingredients) : null;
+    activeFilters.value.missing_ingredients = null;
     activeFilters.value.missing_bar_ingredients = state.filter && state.filter.missing_bar_ingredients ? String(state.filter.missing_bar_ingredients) : null;
     activeFilters.value.ignore_ingredients = state.filter && state.filter.ignore_ingredients ? String(state.filter.ignore_ingredients).split(",") : [];
     activeFilters.value.specific_ingredients = state.filter && state.filter.specific_ingredients ? String(state.filter.specific_ingredients).split(",") : [];
@@ -739,18 +765,22 @@ function stateToQuery() {
         sort: sortWithDir.value,
     };
 
+    if (activeFilters.value.on_shelf) {
+        query.inventory = "1";
+        if (loadedInventoryId.value !== null) {
+            query.inventory_id = String(loadedInventoryId.value);
+        }
+    }
+
     const filters: any = {
         name: searchQuery.value != null && searchQuery.value != "" ? searchQuery.value : null,
-        on_shelf: activeFilters.value.on_shelf,
         bar_shelf: activeFilters.value.bar_shelf,
         locked_bar_cocktails: activeFilters.value.locked_bar_cocktails,
-        locked_user_cocktails: activeFilters.value.locked_user_cocktails,
         favorites: activeFilters.value.favorites,
         is_public: activeFilters.value.is_public,
         user_rating_min: activeFilters.value.user_rating_min ? activeFilters.value.user_rating_min : null,
         average_rating_min: activeFilters.value.average_rating_min ? activeFilters.value.average_rating_min : null,
         total_ingredients: activeFilters.value.total_ingredients ? activeFilters.value.total_ingredients : null,
-        missing_ingredients: activeFilters.value.missing_ingredients ? activeFilters.value.missing_ingredients : null,
         missing_bar_ingredients: activeFilters.value.missing_bar_ingredients ? activeFilters.value.missing_bar_ingredients : null,
         ignore_ingredients: activeFilters.value.ignore_ingredients.length > 0 ? activeFilters.value.ignore_ingredients.join(",") : null,
         tag_id: activeFilters.value.tag_id.length > 0 ? activeFilters.value.tag_id.join(",") : null,
@@ -853,7 +883,7 @@ function handleClickAway(e: any) {
 }
 
 function goToRandomCocktail() {
-    const query = stateToQuery();
+    const query = serverQuery();
     query.per_page = 1;
     query.sort = "random";
 
@@ -888,8 +918,9 @@ onMounted(() => {
 
 watch(
     () => route.query,
-    () => {
+    async () => {
         if (route.name == "cocktails") {
+            await ensureInventoryIngredients();
             queryToState();
             refreshCocktails();
         }
