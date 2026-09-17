67ccd4400c38e858b840708aa633b2f9e225b0f4
skriewel <s.kriewel@gmail.com>
2026-09-18 00:14:29 +0200

    Use publication refinement checkbox filter

--- src/components/Cocktail/CocktailIndex.vue
+++ src/components/Cocktail/CocktailIndex.vue
@@ -74,16 +74,14 @@
                         :refinements="refineAuthors"
                         @change="updateRouterPath"
                     ></Refinement>
-                    <Refinement id="publication" title="Source/Publication">
-                        <input
-                            v-model="activeFilters.publication"
-                            class="form-input"
-                            type="text"
-                            placeholder="Search source or publication..."
-                            @change="updateRouterPath"
-                            @keyup.enter="updateRouterPath"
-                        />
-                    </Refinement>
+                    <Refinement
+                        id="publication"
+                        v-model="activeFilters.publication"
+                        :searchable="true"
+                        title="Source/Publication"
+                        :refinements="refinePublications"
+                        @change="updateRouterPath"
+                    ></Refinement>
                     <Refinement
                         id="favorited-by-user"
                         v-model="activeFilters.favorited_by_user"
@@ -170,7 +168,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
                             <path fill="none" d="M0 0h24v24H0z" />
                             <path
-                                d="M6.17 18a3.001 3.001 0 0 1 5.66 0H22v2H11.83a3.001 3.001 0 0 1-5.66 0H2v-2h4.17zm6-7a3.001 3.001 0 0 1 5.66 0H22v2h-4.17a3.001 3.001 0 0 1-5.66 0H2v-2h10.17zm-6-7a3.001 3.001 0 0 1 5.66 0H22v2H11.83a3.001 3.001 0 0 1-5.66 0H2V4h4.17z"
+                                d="M6.17 18a3.001 3.001 0 0 1 5.66 0H22v2H11.83a3.001 3.001 0 0 1-5.66 0H2v-2h4.17zm6-7a3.001 3.001 0 0 1 5.66 0H22v2h-4.17a3.001 3.001 0 0 1-5.66 0H2v-2h10.17zm-6-7a3.001 3.001 0 0 1 5.66 0H22v2H11.83a3.001 3.001 0 0 1-5.66 0H2v-2h4.17z"
                             />
                         </svg>
                         <div v-show="totalActiveRefinements > 0" class="resource-search__content__filter__count">{{ totalActiveRefinements }}</div>
@@ -211,7 +209,7 @@
                     <button type="button" class="button button--input" :title="$t('cocktail.randomize')" @click.prevent="goToRandomCocktail">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
                             <path
-                                d="M10.9979 1.58018C11.6178 1.22132 12.3822 1.22132 13.0021 1.58018L20.5021 5.92229C21.1197 6.27987 21.5 6.93946 21.5 7.65314V16.3469C21.5 17.0606 21.1197 17.7202 20.5021 18.0778L13.0021 22.4199C12.3822 22.7788 11.6178 22.7788 10.9979 22.4199L3.49793 18.0778C2.88029 17.7202 2.5 17.0606 2.5 16.3469V7.65314C2.5 6.93947 2.88029 6.27987 3.49793 5.92229L10.9979 1.58018ZM4.5 7.65314V7.65792L11.0021 11.4223C11.6197 11.7799 12 12.4395 12 13.1531V20.689L19.5 16.3469V7.65314L12 3.31104L4.5 7.65314ZM6.13208 12.3C6.13206 11.7477 5.74432 11.0761 5.26604 10.7999C4.78776 10.5238 4.40004 10.7476 4.40006 11.2999C4.40008 11.8522 4.78782 12.5238 5.2661 12.7999C5.74439 13.0761 6.1321 12.8523 6.13208 12.3ZM8.72899 18.7982C9.20728 19.0743 9.59499 18.8505 9.59497 18.2982C9.59495 17.7459 9.20721 17.0743 8.72893 16.7982C8.25065 16.522 7.86293 16.7459 7.86295 17.2982C7.86297 17.8504 8.25071 18.522 8.72899 18.7982ZM5.2661 16.799C5.74439 17.0751 6.1321 16.8513 6.13208 16.299C6.13206 15.7467 5.74432 15.0751 5.26604 14.799C4.78776 14.5228 4.40004 14.7467 4.40006 15.2989C4.40008 15.8512 4.78782 16.5228 5.2661 16.799ZM8.72851 14.7995C9.20679 15.0756 9.5945 14.8518 9.59448 14.2995C9.59446 13.7472 9.20673 15.0756 8.72844 14.7995C8.25016 14.5233 7.86245 14.7471 7.86246 15.2994C7.86248 15.8517 8.25022 16.5233 8.72851 16.7995ZM14.8979 8.00001C15.3762 7.72388 15.3762 7.27619 14.8979 7.00006C14.4196 6.72394 13.6441 6.72394 13.1658 7.00006C12.6875 7.27619 12.6875 7.72388 13.1658 8.00001C13.6441 8.27614 14.4196 8.27614 14.8979 8.00001ZM10.0981 7.00006C10.5764 7.27619 10.5764 7.72388 10.0981 8.00001C9.61982 8.27614 8.84434 8.27614 8.36604 8.00001C7.88774 7.72388 7.88774 7.27619 8.36604 7.00006C8.84434 6.72394 9.61982 6.72394 10.0981 7.00006ZM15.9954 15.3495C16.5932 15.0043 17.0779 14.1649 17.0779 13.4745C17.0779 12.7842 16.5933 12.5044 15.9955 12.8496C15.3977 13.1948 14.9131 14.0342 14.913 14.7246C14.913 15.4149 15.3976 15.6947 15.9954 15.3495Z"
+                                d="M10.9979 1.58018C11.6178 1.22132 12.3822 1.22132 13.0021 1.58018L20.5021 5.92229C21.1197 6.27987 21.5 6.93946 21.5 7.65314V16.3469C21.5 17.0606 21.1197 17.7202 20.5021 18.0778L13.0021 22.4199C12.3822 22.7788 11.6178 22.7788 10.9979 22.4199L3.49793 18.0778C2.88029 17.7202 2.5 17.0606 2.5 16.3469V7.65314C2.5 6.93947 6.23858 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM4.5 7.65314V7.65792L11.0021 11.4223C11.6197 11.7799 12 12.4395 12 13.1531V20.689L19.5 16.3469V7.65314L12 3.31104L4.5 7.65314Z"
                             ></path>
                         </svg>
                     </button>
@@ -283,7 +281,7 @@
                         <template #icon>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32">
                                 <path
-                                    d="M11 2C15.968 2 20 6.032 20 11C20 15.968 15.968 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2ZM11 18C14.8675 18 18 14.8675 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18ZM19.4853 18.0711L22.3137 20.8995L20.8995 22.3137L18.0711 19.4853L19.4853 18.0711Z"
+                                    d="M11 2C15.968 2 20 6.032 20 11C20 15.968 15.968 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2ZM11 18C14.8675 18 18 14.8675 18 11C18 7.1325 14.8675 4 11 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM19.4853 18.0711L22.3137 20.8995L20.8995 22.3137L18.0711 19.4853L19.4853 18.0711Z"
                                 ></path>
                             </svg>
                         </template>
@@ -352,7 +350,7 @@ interface ActiveFilters {
     user_shelves: string[];
     created_user_id: string[];
     author: string[];
-    publication: string | null;
+    publication: string[];
     favorited_by_user: string[];
     ignore_ingredients: string[];
     specific_ingredients: string[];
@@ -376,6 +374,7 @@ interface AvailableRefinements {
     shared_collections: any[];
     members: any[];
     authors: { name: string }[];
+    publications: { name: string }[];
 }
 
 const route = useRoute();
@@ -414,6 +413,7 @@ const availableRefinements = ref<AvailableRefinements>({
     shared_collections: [],
     members: [],
     authors: [],
+    publications: [],
 });
 
 const activeFilters = ref<ActiveFilters>({
@@ -439,7 +439,7 @@ const activeFilters = ref<ActiveFilters>({
     user_shelves: [],
     created_user_id: [],
     author: [],
-    publication: null,
+    publication: [],
     favorited_by_user: [],
     ignore_ingredients: [],
     specific_ingredients: [],
@@ -565,6 +565,14 @@ const refineAuthors = computed(() => {
         id: a.name,
         value: a.name,
         name: a.name,
+    }));
+});
+
+const refinePublications = computed(() => {
+    return availableRefinements.value.publications.map((publication: any) => ({
+        id: publication.name,
+        value: publication.name,
+        name: publication.name,
     }));
 });
 
@@ -708,6 +716,7 @@ function refreshCocktails() {
             cocktails.value = resp?.data ?? [];
             meta.value = resp?.meta;
             availableRefinements.value.authors = resp?.meta?.filters?.authors ?? [];
+            availableRefinements.value.publications = resp?.meta?.filters?.publications ?? [];
             isLoading.value = false;
         })
         .catch((e) => {
@@ -733,7 +742,7 @@ function queryToState() {
     activeFilters.value.user_shelves = state.filter && state.filter.user_shelves ? String(state.filter.user_shelves).split(",") : [];
     activeFilters.value.created_user_id = state.filter && state.filter.created_user_id ? String(state.filter.created_user_id).split(",") : [];
     activeFilters.value.author = state.filter && state.filter.author ? String(state.filter.author).split(",") : [];
-    activeFilters.value.publication = state.filter && state.filter.publication ? String(state.filter.publication) : null;
+    activeFilters.value.publication = state.filter && state.filter.publication ? String(state.filter.publication).split(",") : [];
     activeFilters.value.favorited_by_user = state.filter && state.filter.favorited_by_user ? String(state.filter.favorited_by_user).split(",") : [];
     activeFilters.value.on_shelf = String(state.inventory ?? "") === "1";
     activeFilters.value.bar_shelf = state.filter && state.filter.bar_shelf ? state.filter.bar_shelf : null;
@@ -808,7 +817,7 @@ function stateToQuery() {
         id: activeFilters.value.id.length > 0 ? activeFilters.value.id.join(",") : null,
         created_user_id: activeFilters.value.created_user_id.length > 0 ? activeFilters.value.created_user_id.join(",") : null,
         author: activeFilters.value.author.length > 0 ? activeFilters.value.author.join(",") : null,
-        publication: activeFilters.value.publication && activeFilters.value.publication.trim() !== "" ? activeFilters.value.publication.trim() : null,
+        publication: activeFilters.value.publication.length > 0 ? activeFilters.value.publication.join(",") : null,
         favorited_by_user: activeFilters.value.favorited_by_user.length > 0 ? activeFilters.value.favorited_by_user.join(",") : null,
         abv_min: activeFilters.value.abv ? activeFilters.value.abv.min : null,
         abv_max: activeFilters.value.abv ? activeFilters.value.abv.max : null,
@@ -877,7 +886,7 @@ function clearRefinements() {
         user_shelves: [],
         created_user_id: [],
         author: [],
-        publication: null,
+        publication: [],
         favorited_by_user: [],
         ignore_ingredients: [],
         specific_ingredients: [],
