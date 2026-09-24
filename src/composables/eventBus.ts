570eea0dcb1c3b3d1c667e34cb0a9de15ecbac27
skriewel <s.kriewel@gmail.com>
2026-09-24 20:34:22 +0200

    Add cocktail tap event bus

--- src/composables/eventBus.ts
+++ src/composables/eventBus.ts
@@ -2,5 +2,6 @@ import { useEventBus } from "@vueuse/core";
 
 const dialogBus = useEventBus<string>("dialogs");
 const barBus = useEventBus<string>("bars");
+const cocktailTapBus = useEventBus<number>("cocktail-taps");
 
-export { dialogBus, barBus };
+export { dialogBus, barBus, cocktailTapBus };
