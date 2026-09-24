2f19c8b2c4c5b3ab8467b28082d2343673098c65
skriewel <s.kriewel@gmail.com>
2026-09-24 20:30:42 +0200

    Remove obsolete tap filter component reference

--- src/views/CocktailsView.vue
+++ src/views/CocktailsView.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import CocktailIndex from "../components/Cocktail/CocktailIndex.vue";
-import CocktailTapListFilters from "../components/Cocktail/CocktailTapListFilters.vue";
 </script>
 
 <template>
     <main>
         <CocktailIndex />
-        <CocktailTapListFilters />
     </main>
 </template>
